2079232f5df9d68d1e92f388b1ec28e2730b7719
Kaique Galera <kaiquegalera@gmail.com>
2026-08-16 19:52:04 -0300

    feat: cria variaveis e logica para encerrar o jogo

--- app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboardcompose/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboardcompose/MainActivity.kt
@@ -1,20 +1,28 @@
 package br.edu.ifsp.scl.sc3046699.trucoscoreboardcompose
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    private var pontosA = 0
+    private var pontosB = 0
+
+    private lateinit var textPontosA: TextView
+    private lateinit var textPontosB: TextView
+    private lateinit var textMensagem: TextView
+
+    private lateinit var buttonMais1A: Button
+    private lateinit var buttonMais3A: Button
+    private lateinit var buttonMais1B: Button
+    private lateinit var buttonMais3B: Button
+    private lateinit var buttonReiniciar: Button
+
+    private fun jogoFinalizado(): Boolean {
+        return pontosA >= 12 || pontosB >= 12
     }
 }
